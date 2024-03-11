512e64b426f9bf672cb4a57c401e29ccda106221
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-10 23:30:31 -0500

    completed the mutations file for client

--- client/src/js/mutations.js
+++ client/src/js/mutations.js
@@ -1,9 +1,68 @@
-// Meet these requirements:
-// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+import { gql } from '@apollo/client';
 
-// ADD_USER will execute the addUser mutation.
+// Mutation for logging in a user
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-// SAVE_BOOK will execute the saveBook mutation.
+// Mutation for adding a new user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-// REMOVE_BOOK will execute the removeBook mutation.
+// Mutation for saving a book
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+// Mutation for removing a book from saved list
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
 
