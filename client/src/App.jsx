9af279d1506739f5df25f60aaa4f71cfdc6fec00
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-10 23:20:23 -0500

    updated app.jsx for apollo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import client from './apolloClient';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
