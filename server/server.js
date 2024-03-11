2c84347eb91eec6dca7c66e1197019de8aca485b
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-10 22:42:38 -0500

    added a schemas folder with index.js file completed

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const routes = require('./routes');
